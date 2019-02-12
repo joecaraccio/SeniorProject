ee0cf1acc73928eec570644e210892fdba687c49
Joe Caraccio <jacaraccio@gmail.com>
2019-02-11 21:06:33 -0500

    Blank Project in Progress

---
+++ Sandbox/Sandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Urho;
+
+namespace Sandbox
+{
+    class MainClass : Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
