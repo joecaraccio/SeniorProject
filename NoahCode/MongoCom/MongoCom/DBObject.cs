357f075d4dd28609801fe6bebe5178b17dca4119
Noah <nzf1000@wildcats.unh.edu>
2019-02-12 16:02:59 -0500

    Fix Structure
    Fix structure so that everything there

---
+++ NoahCode/MongoCom/MongoCom/DBObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoCom
+{
+    public class DBObject
+    {
+        private BsonDocument document;
+        public DBObject(ObjectId id, string name, string type, float x, float y, float z, float rx, float ry, float rz, float sx, float sy, String[] info)
+        {
+            document = new BsonDocument
+            {
+                {"tourID", id },
+                {"tourName", new BsonString(name) },
+                {"objType", new BsonString(type) },
+                {"x-coordinate", new BsonString(x.ToString()) },
+                {"y-coordinate", new BsonString(y.ToString()) },
+                {"z-coordinate", new BsonString(z.ToString()) },
+                {"rotationX", new BsonString(rx.ToString()) },
+                {"rotationY", new BsonString(ry.ToString()) },
+                {"ScaleX", new BsonString(sx.ToString()) },
+                {"ScaleY", new BsonString(sx.ToString()) },
+                {"info", new BsonArray(info) }
+            };
+        }
+        public BsonDocument returnDoc()
+        {
+            return document;
+        }
+    }
+}
---
+++ NoahCode/MongoCom/MongoCom/HoloTourCom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoCom
+{
+    public class HoloTourCom
+    {
+        private ObjectId tourID;
+        private string tourName;
+        private string objType;
+        private float objLocX;
+        private float objLocY;
+        private float objLocZ;
+        private float rotationX;
+        private float rotationY;
+        private float rotationZ;
+        private float scaleX;
+        private float scaleY;
+        
+        public 
+            HoloTourCom(String tourName)
+        {
+            string connectionString = "mongodb://admin:admin1@ds157574.mlab.com:57574";
+            var dbClient = new MongoClient(connectionString);
+            IMongoDatabase dataBase = dbClient.GetDatabase("holotours");
+            var collection = dataBase.GetCollection<BsonDocument>("tours");
+            this.tourName = tourName;
+            this.tourID = ObjectId.GenerateNewId();
+            var idDoc = new BsonDocument
+            {
+                {"tourID", tourID },
+                {"tourName", new BsonString(tourName) }
+            };
+            
+        }
+
+        private void setObject()
+        {
+            string connectionString = "mongodb://admin:admin1@ds157574.mlab.com:57574";
+            var dbClient = new MongoClient(connectionString);
+            IMongoDatabase dataBase = dbClient.GetDatabase("holotours");
+            
+        }
+        private String[] getObjects()
+        {
+            string[] temp = { "", "", "" };
+            string connectionString = "mongodb://admin:admin1@ds157574.mlab.com:57574";
+            var dbClient = new MongoClient(connectionString);
+            var session = dbClient.StartSession();
+            IMongoDatabase dataBase = dbClient.GetDatabase("holotours");
+            var collection = dataBase.GetCollection<BsonDocument>("test");
+            var filter = new BsonDocument("ProjectID", tourID);
+            collection.FindSync(filter).ToList;
+
+            return temp;
+        }
+    }
+}
